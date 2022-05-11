58f3083aa6d28f6c3bf0a81552b640b6cfcc450b
shagors <shajjadshagor@gmail.com>
2022-05-11 09:29:52 +0600

    set up and install all requirements library mongo cors express and dotenv

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dthae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+app.get('/', (req, res) => {
+    res.send('Server Connect from Doctor')
+})
+
+app.listen(port, () => {
+    console.log(`Doctors Portal is listen from ${port}`);
+})
