6925287138900f332f8f19f29930cb5143a4b83f
shagors <shajjadshagor@gmail.com>
2022-05-16 00:05:52 +0600

    put data set for client side

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run(){
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
         const bookingCollection = client.db('doctors_portal').collection('bookings');
+        const userCollection = client.db('doctors_portal').collection('user');
 
         app.get('/service', async(req, res) => {
             const query = '';
@@ -24,6 +25,18 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
         });
+
+        app.put('/user/:email', async(req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email: email};
+            const options = { upsert: true};
+            const updateDoc = {
+                $set: user,
+            };
+            const result= await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
 
         app.get('/available', async(req, res) => {
             const date = req.query.date;
