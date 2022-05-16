96876e27b54c21aafadb681ec11754ad54193d3c
shagors <shajjadshagor@gmail.com>
2022-05-16 23:06:09 +0600

    JWT token verify and email verification... validate check for user email

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
@@ -11,6 +12,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dthae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// verify JWT token for user
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'UnAuthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'});
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run(){
     try{
@@ -26,6 +43,11 @@ async function run(){
             res.send(services);
         });
 
+        app.get('/user', async(req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
         app.put('/user/:email', async(req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -35,7 +57,8 @@ async function run(){
                 $set: user,
             };
             const result= await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({result, token});
         })
 
         app.get('/available', async(req, res) => {
@@ -71,11 +94,18 @@ async function run(){
          * app.delete('/booking/:id) //
          */
 
-        app.get('/booking', async(req, res) => {
+        app.get('/booking', verifyJWT, async(req, res) => {
             const patient = req.query.patient;
-            const query = {patient: patient};
-             const bookings = await bookingCollection.find(query).toArray();
-             res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if(patient === decodedEmail){
+                const authorization = req.headers.authorization;
+                const query = {patient: patient};
+                 const bookings = await bookingCollection.find(query).toArray();
+                 return res.send(bookings);
+            }
+            else{
+                return res.status(403).send({message: 'Forbidden Access'});
+            }
         })
 
         app.post('/booking', async(req, res) => {
